df5ab78d7c83ee0d7ac99229716fe370912e15a9
Cheyu Tu <cheyut@andrew.cmu.edu>
2024-02-06 19:35:07 -0500

    added scope & method to Child

--- app/models/child.rb
+++ app/models/child.rb
@@ -4,4 +4,11 @@ class Child < ApplicationRecord
 
     validates_presence_of :first_name
     validates_presence_of :last_name
+
+    scope :alphabetical, -> {order(:last_name, :first_name)}
+    scope :active, -> { where(active: true) }
+
+    def name
+        "#{first_name} #{last_name}"
+    end
 end
