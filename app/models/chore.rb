164fe3df419b05945113f9062b1201203c3eaa22
Cheyu Tu <cheyut@andrew.cmu.edu>
2024-02-06 20:20:01 -0500

    chore test

--- app/models/chore.rb
+++ app/models/chore.rb
@@ -1,4 +1,21 @@
 class Chore < ApplicationRecord
     belongs_to :child
     belongs_to :task
+
+    validates :due_on, timeliness: { type: :date }
+
+    scope :by_task, ->{ joins(:task).order('name') }
+    scope :chronological, -> { order(:due_on) }
+    scope :pending, -> {where(completed: false)}
+    scope :done, -> {where(completed: true)}
+    scope :upcoming, ->{where('due_on >= ?', Date.today)}
+    scope :past, -> {where('due_on < ?', Date.today)}
+
+    def status
+        if completed
+            return 'Completed'
+        else
+            return 'Pending'
+        end
+    end
 end
